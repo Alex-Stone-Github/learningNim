45056d90f1c9fef7868c9b499dc9abd67c49bfff
Alex Stone <stonealex145@gmail.com>
2022-08-28 13:34:00 -0500

    Guessing game

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # learningNim
 I heard about a new programming language called nim and I wanted to test it out
+
+
+Also this is my first time and am basing all of this code off of code examples that I have found, so sorry all for bad programming habits and techniques.
+
+
+I love the syntax - its kinda like a low level python
--- main.nim
+++ main.nim
@@ -1,1 +1,21 @@
-echo "Hello, World!"
+import std/random
+import strutils
+
+# I love how the syntax is like python2s almost
+
+# call library constructors - I think
+randomize() # random
+
+var target = rand(0..5)
+var guess = 6
+
+echo target
+echo "Guess a number between 1, and 5"
+while guess != target:
+    echo "Guess Again"
+    guess = parseInt(readLine(stdin))
+    if guess > target:
+        echo "Guess Lower"
+    if guess < target:
+        echo "Guess Higher"
+echo "Congrats, you guess the correct number"
